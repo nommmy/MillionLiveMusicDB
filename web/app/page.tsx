d97f70b74ffb73092f99d06b352fdcb7e8778da0
nomiyama <nomiyamade@gmail.com>
2024-07-07 11:52:28 +0900

    DB更新cron時にオンデマンドで再生成するように変更

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,8 +2,6 @@ import Ranking from "./_components/ranking/Ranking";
 import AlbumCardGrid from "./_components/album/AlbumCardGrid";
 import CharacterGrid from "./_components/characters/CharacterGrid";
 
-export const revalidate = 86400;
-
 export default async function Home() {
   return (
     <main className="main">
